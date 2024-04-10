bc78691b5239c57ed45bb0ed45b69c816725de49
Meiria Dwi Saputri <127822520+meiriadwi@users.noreply.github.com>
2024-04-10 08:25:11 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-0c057a65-98b2-4d84-bd54-6f0494575234/)
 
 
 echo "$curl_output" >> Update.tsx
